53470de52b3b1ed46a4c33d3c1645c1748fe8819
scottgl@gmail.com <scottgl@gmail.com>
2017-10-18 11:54:56 -0500

    Updated swbd1_merge.sh in tedlium_swbd.

--- egs/tedlium_swbd/s5/local/swbd1_merge.sh
+++ egs/tedlium_swbd/s5/local/swbd1_merge.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-glm  reco2file_and_channel  segments  spk2utt  stm  text  utt2dur  utt2spk  wav.scp
+mv data/train.orig/wav.scp data/train.orig/wav.scp.orig
+mv data/train.orig/stm data/train.orig/stm.orig
+mv data/train.orig/text data/train.orig/text.orig
+mv data/train.orig/segments data/train.orig/segments.orig
+mv data/train.orig/spk2utt data/train.orig/spk2utt.orig
+mv data/train.orig/utt2spk data/train.orig/utt2spk.orig
+mv data/train.orig/reco2file_and_channel data/train.orig/reco2file_and_channel.orig
 
-cat db/wav.scp data/train.orig/wav.scp > data/train.orig/wav.scp
-cat db/stm data/train.orig/stm > data/train.orig/stm
-cat db/text data/train.orig/text > data/train.orig/text
-cat db/segments data/train.orig/segments > data/train.orig/segments
-cat db/spk2utt data/train.orig/spk2utt > data/train.orig/spk2utt
-cat db/utt2spk data/train.orig/utt2spk > data/train.orig/utt2spk
-cat db/reco2file_and_channel data/train.orig/reco2file_and_channel > data/train.orig/reco2file_and_channel
+cat db/wav.scp data/train.orig/wav.scp.orig > data/train.orig/wav.scp
+cat db/stm data/train.orig/stm.orig > data/train.orig/stm
+cat db/text data/train.orig/text.orig > data/train.orig/text
+cat db/segments data/train.orig/segments.orig > data/train.orig/segments
+cat db/spk2utt data/train.orig/spk2utt.orig > data/train.orig/spk2utt
+cat db/utt2spk data/train.orig/utt2spk.orig > data/train.orig/utt2spk
+cat db/reco2file_and_channel data/train.orig/reco2file_and_channel.orig > data/train.orig/reco2file_and_channel
 
-
+#sort data/train.orig/wav.scp
+sort data/train.orig/segments > data/train.orig/segments
+sort data/train.orig/spk2utt > data/train.orig/spk2utt
+sort data/train.orig/utt2spk > data/train.orig/utt2spk
+sort data/train.orig/reco2file_and_channel > data/train.orig/reco2file_and_channel
 
 
 
